65b0b7b4100ecdaec71ede9a14a8901a562cd13a
Sanjana2493 <47670506+Sanjana2493@users.noreply.github.com>
2019-02-16 14:43:20 -0600

    Initial Commit

---
+++ Report_reader.py
@@ -0,0 +1,59 @@
+from tika import parser
+
+import os
+path = 'File path in local dict.'
+
+key_words = ['data analytics','big data', 'retail', 'analytics', 'business analytics', 'business intelligence', 'big data analytics', 'blockchain', 'artificial intelligence', 'machine learning', 'data science', 'data scientist', 'IoT']
+
+import xlwt
+workBook = xlwt.Workbook()
+workSheet = workBook.add_sheet("Name of WorkSheet")
+
+col = 1
+
+workSheet.write(0,0," ")
+
+for word in key_words:
+    workSheet.write(0,col,word)
+    col += 1
+    
+import string
+p = string.punctuation
+d = string.digits
+table = str.maketrans(p,len(p)* ' ')
+table1 = str.maketrans(d,len(d)* ' ')
+
+import nltk
+stopwords = nltk.corpus.stopwords.words("english")
+
+row = 1
+row1 = 1
+
+for path,dirs,files in os.walk(path):
+    content_str = ''
+    for file in files:
+        print(file,':')
+        company = file.split('.')
+        print(company[0])
+        workSheet.write(row,0,company[0])
+        file_path = os.path.join(path,file)
+        parsedPDF = parser.from_file(file_path)
+        content = parsedPDF['content']
+        content_edit = content.translate(table)
+        content_edit1 = content_edit.translate(table1)
+        content_edit2 = content_edit1.lower()
+        content_words = content_edit2.split()
+        col1 = 1
+        for word in key_words:
+            counter = content_words.count(word)
+            workSheet.write(row1,col1,str(counter))
+            if(counter > 0):
+                print(word , 'is present')
+            else:
+                print(word , 'is not present')
+            col1 += 1
+        print()
+        row += 1
+        row1 += 1
+
+workBook.save("Name of Spreadsheet.xls")
